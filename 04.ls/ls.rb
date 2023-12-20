d9741b1351c574c26e946abc0335bccb79295a84
ryufuta <rfutago@gmail.com>
2023-12-20 12:24:43 +0900

    列数の仕様変更に対応できるロジックに修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -13,14 +13,16 @@ file_names = file_names.map do |file_name|
   file_name.ljust(column_width - n_full_widths)
 end
 
-# ファイル数が３の倍数になるように末尾に空文字追加
-file_names += [''] * (3 - file_names.size % 3) unless (file_names.size % 3) == 0
+# ファイル数が列数の倍数になるように末尾に空文字追加
+n_columns = 3
+file_names += [''] * (n_columns - file_names.size % n_columns) unless (file_names.size % n_columns) == 0
 
-# 上から下、左から右へ昇順、3列になるように表示
+# 上から下、左から右へ昇順、指定した列数になるように表示
 ls_text = ''
-n_rows = file_names.size / 3
+n_rows = file_names.size / n_columns
 n_rows.times do |row|
-  ls_text += "#{file_names[row]}#{file_names[row + n_rows]}#{file_names[row + n_rows * 2]}\n"
+  n_columns.times { |col| ls_text += file_names[row + n_rows * col] }
+  ls_text += "\n"
 end
 
 puts ls_text
