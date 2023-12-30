1ee065c473d9fa56cba78fd72e86369c05f35d57
ryufuta <rfutago@gmail.com>
2023-12-30 15:31:27 +0900

    ファイル名の長さによって列幅が変わるよう修正
    ファイル名が長いと正しく表示されないバグがあったため
    列幅の値はmacOS Ventura, zshに合わせた

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+COLUMN_WIDTH_UNIT = 8
+
 def main
   file_names = Dir.glob('*')
 
@@ -10,13 +12,20 @@ def main
   puts to_ls_text(file_names)
 end
 
-def justify_columns(file_names, column_width = 16)
-  # 指定した半角文字数の列幅にする。全角文字は2文字分の幅のためその分短くする
-  file_names.map do |file_name|
-    # 全角文字をいくつ含むか
-    n_full_widths = file_name.scan(/[^\x01-\x7E\uFF65-\uFF9F]/).size
-    file_name.ljust(column_width - n_full_widths)
-  end
+def justify_columns(file_names)
+  # 全角文字は2文字分カウント
+  max_name_length = file_names.map { |file_name| file_name.size + count_full_width_chars(file_name) }.max
+
+  column_width = max_name_length + 1
+  residue = column_width % COLUMN_WIDTH_UNIT
+  column_width += COLUMN_WIDTH_UNIT - residue unless residue.zero?
+
+  # 全角文字は2文字分の幅のためその分短くする
+  file_names.map { |file_name| file_name.ljust(column_width - count_full_width_chars(file_name)) }
+end
+
+def count_full_width_chars(str)
+  str.scan(/[^\x01-\x7E\uFF65-\uFF9F]/).size
 end
 
 def to_ls_text(file_names, n_columns = 3)
