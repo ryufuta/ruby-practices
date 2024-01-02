defb2018a41174e57dd07b288a83490c000e44c2
ryufuta <rfutago@gmail.com>
2024-01-02 17:40:14 +0900

    min_multipleメソッドの名前とロジックをわかりやすくした

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -17,7 +17,7 @@ def justify_columns(file_names)
   # 全角文字は2文字分カウント
   max_name_length = file_names.map { |file_name| file_name.size + count_full_width_chars(file_name) }.max
 
-  column_width = min_multiple(max_name_length + 1, COLUMN_WIDTH_UNIT)
+  column_width = ceil_to_multiple(max_name_length + 1, COLUMN_WIDTH_UNIT)
 
   # 全角文字は2文字分の幅のためその分短くする
   file_names.map { |file_name| file_name.ljust(column_width - count_full_width_chars(file_name)) }
@@ -29,7 +29,7 @@ end
 
 def to_ls_text(file_names)
   # ファイル数が列数の倍数になるように末尾に空文字追加
-  file_names += [''] * (min_multiple(file_names.size, N_COLUMNS) - file_names.size)
+  file_names += [''] * (ceil_to_multiple(file_names.size, N_COLUMNS) - file_names.size)
 
   # 上から下、左から右へ昇順、指定した列数になるように表示
   ls_text = ''
@@ -41,9 +41,8 @@ def to_ls_text(file_names)
   ls_text
 end
 
-def min_multiple(number, divisor)
-  residue = number % divisor
-  residue.zero? ? number : number + divisor - residue
+def ceil_to_multiple(number, divisor)
+  number.ceildiv(divisor) * divisor
 end
 
 main if __FILE__ == $PROGRAM_NAME
