1ed17b407280a6698052200afb6469a0caf46120
ryufuta <rfutago@gmail.com>
2024-02-19 22:22:22 +0900

    lwcオプションに対応

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -1,22 +1,34 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'optparse'
+
 COLUMN_WIDTH = 8
 
 def main
+  options = ARGV.getopts('lwc')
+  use_counts = parse_options(options)
   file_paths = ARGV
   counts_by_row =
     if file_paths.empty?
       lines = $stdin.readlines
-      [[nil, count_lines_words_bytes(lines)]]
+      [[nil, count_lines_words_bytes(lines, **use_counts)]]
     else
-      counts_by_file = file_paths.map { |file_path| [file_path, count_lines_words_bytes(file_path)] }
+      counts_by_file = file_paths.map { |file_path| [file_path, count_lines_words_bytes(file_path, **use_counts)] }
       counts_by_file.size > 1 ? [*counts_by_file, ['total', sum_counts(counts_by_file)]] : counts_by_file
     end
   puts to_wc_text(counts_by_row)
 end
 
-def count_lines_words_bytes(file_path_or_lines)
+def parse_options(options)
+  if !options['l'] && !options['w'] && !options['c']
+    { use_line: true, use_word: true, use_byte: true }
+  else
+    { use_line: options['l'], use_word: options['w'], use_byte: options['c'] }
+  end
+end
+
+def count_lines_words_bytes(file_path_or_lines, use_line: true, use_word: true, use_byte: true)
   lines =
     if file_path_or_lines.instance_of?(String)
       File.open(file_path_or_lines, 'r', &:readlines)
@@ -24,15 +36,15 @@ def count_lines_words_bytes(file_path_or_lines)
       file_path_or_lines
     end
 
-  [
-    lines.count,
-    lines.sum { |line| line.split(/\s+/).count },
-    lines.sum(&:bytesize)
-  ]
+  counts = []
+  counts << lines.count if use_line
+  counts << lines.sum { |line| line.split(/\s+/).count } if use_word
+  counts << lines.sum(&:bytesize) if use_byte
+  counts
 end
 
 def sum_counts(counts_by_file)
-  total_counts = [0, 0, 0]
+  total_counts = [0] * counts_by_file[0][1].size
   counts_by_file.each do |_, counts|
     counts.each_with_index { |count, i| total_counts[i] += count }
   end
