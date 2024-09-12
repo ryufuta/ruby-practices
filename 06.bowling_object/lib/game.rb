a8e4fdcc71ac75ac5e8fa57f69baa9f821e7c2c4
ryufuta <rfutago@gmail.com>
2024-09-12 16:41:55 +0900

    Implement Game class

---
+++ 06.bowling_object/lib/game.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require_relative 'frame'
+
+class Game
+  def initialize(marks_text)
+    marks_by_frame = self.class.parse_marks(marks_text)
+    @frames = marks_by_frame.map { |marks| Frame.new(*marks) }
+  end
+
+  def score
+    @frames.each_with_index.sum do |frame, i|
+      final_frame?(i) ? frame.score : score_non_final(i)
+    end
+  end
+
+  def final_frame?(idx)
+    idx == 9
+  end
+
+  def score_non_final(idx)
+    frame = @frames[idx]
+    frame.score + score_bonus_shots(idx)
+  end
+
+  def score_bonus_shots(idx)
+    frame = @frames[idx]
+    next_frame = @frames[idx + 1]
+    if strike?(frame)
+      next_frame.first_shot.score + (next_frame.second_shot.mark.nil? ? @frames[idx + 2].first_shot.score : next_frame.second_shot.score)
+    elsif spare?(frame)
+      next_frame.first_shot.score
+    else
+      0
+    end
+  end
+
+  def strike?(frame)
+    frame.first_shot.mark == 'X'
+  end
+
+  def spare?(frame)
+    frame.first_shot.mark != 'X' && frame.score == 10
+  end
+
+  def self.parse_marks(marks_text)
+    marks = marks_text.split(',')
+    marks_by_frame = []
+    9.times do
+      marks_by_frame << if marks.first == 'X'
+                          marks.shift(1)
+                        else
+                          marks.shift(2)
+                        end
+    end
+    marks_by_frame << marks
+  end
+end
---
+++ 06.bowling_object/test/game_test.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../lib/game'
+
+class GameTest < Minitest::Test
+  def test_parse_marks_two_shots_per_frame
+    marks_by_frame = Game.parse_marks('1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5')
+    assert_equal [%w[1 5]] * 10, marks_by_frame
+  end
+
+  def test_parse_marks_one_or_three_shots_per_frame
+    marks_by_frame = Game.parse_marks('X,X,X,X,X,X,X,X,X,X,X,X')
+    expected = [['X']] * 9 + [['X'] * 3]
+    assert_equal expected, marks_by_frame
+  end
+
+  def test_score_no_bonus
+    game = Game.new('1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5')
+    assert_equal 60, game.score
+  end
+
+  def test_score_no_strike
+    game = Game.new('1,9,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,5,1,9,1')
+    assert_equal 70, game.score
+  end
+
+  # 提出用と同じテストケース
+  def test_score1
+    game = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5')
+    assert_equal 139, game.score
+  end
+
+  def test_score2
+    game = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X')
+    assert_equal 164, game.score
+  end
+
+  def test_score3
+    game = Game.new('0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4')
+    assert_equal 107, game.score
+  end
+
+  def test_score4
+    game = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0')
+    assert_equal 134, game.score
+  end
+
+  def test_score5
+    game = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8')
+    assert_equal 144, game.score
+  end
+
+  def test_score6_perfect
+    game = Game.new('X,X,X,X,X,X,X,X,X,X,X,X')
+    assert_equal 300, game.score
+  end
+
+  def test_score7
+    game = Game.new('X,X,X,X,X,X,X,X,X,X,X,2')
+    assert_equal 292, game.score
+  end
+
+  def test_score8
+    game = Game.new('X,0,0,X,0,0,X,0,0,X,0,0,X,0,0')
+    assert_equal 50, game.score
+  end
+end
