409dad10b0890da827bb1aebd35351a3a30e08a6
ryufuta <rfutago@gmail.com>
2024-09-12 16:41:12 +0900

    Implement Frame class

---
+++ 06.bowling_object/test/frame_test.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../lib/frame'
+
+class FrameTest < Minitest::Test
+  def test_score_strike
+    frame = Frame.new('X')
+    assert_equal 10, frame.score
+  end
+
+  def test_score_two_shots
+    frame = Frame.new('0', '10')
+    assert_equal 10, frame.score
+  end
+
+  def test_score_three_shots
+    frame = Frame.new('X', 'X', '1')
+    assert_equal 21, frame.score
+  end
+end
