553bfe49a47dbe11459bc4db1fd2b0c433345062
ryufuta <rfutago@gmail.com>
2024-09-14 22:14:29 +0900

    Remove useless attr_reader in Frame class

--- 06.bowling_object/lib/frame.rb
+++ 06.bowling_object/lib/frame.rb
@@ -3,8 +3,6 @@
 require_relative 'shot'
 
 class Frame
-  attr_reader :first_shot, :second_shot, :third_shot
-
   def initialize(first_mark, second_mark = nil, third_mark = nil)
     @first_shot = Shot.new(first_mark)
     @second_shot = Shot.new(second_mark)
