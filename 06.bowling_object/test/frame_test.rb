d7f4a56a4aad2dd22697b7569bd9c059be3a95dd
ryufuta <rfutago@gmail.com>
2024-09-27 15:52:08 +0900

    Improve readability and performance of tests in `frame_test.rb`

--- 06.bowling_object/test/frame_test.rb
+++ 06.bowling_object/test/frame_test.rb
@@ -5,9 +5,8 @@ require_relative '../lib/frame'
 
 class FrameTest < Minitest::Test
   def test_score_no_bonus
-    frames = []
-    10.times do |idx|
-      frames << (idx == 9 ? Frame.new(idx, 'X', 'X', '1') : Frame.new(idx, '1', '5'))
+    frames = Array.new(10) do |idx|
+      idx == 9 ? Frame.new(idx, 'X', 'X', '1') : Frame.new(idx, '1', '5')
     end
 
     frame_with_two_shots = frames.first
@@ -18,42 +17,38 @@ class FrameTest < Minitest::Test
   end
 
   def test_score_spare
-    frames = []
-    10.times do |idx|
-      frames << (idx.zero? ? Frame.new(idx, '1', '9') : Frame.new(idx, '1', '5'))
+    frames = Array.new(10) do |idx|
+      idx.zero? ? Frame.new(idx, '1', '9') : Frame.new(idx, '1', '5')
     end
 
     assert_equal 11, frames.first.score(frames)
   end
 
   def test_score_single_strike
-    frames = []
-    10.times do |idx|
-      frames << (idx.zero? ? Frame.new(idx, 'X') : Frame.new(idx, '1', '5'))
+    frames = Array.new(10) do |idx|
+      idx.zero? ? Frame.new(idx, 'X') : Frame.new(idx, '1', '5')
     end
 
     assert_equal 16, frames.first.score(frames)
   end
 
   def test_score_double_strike
-    frames = []
-    10.times do |idx|
-      frames << (idx < 2 ? Frame.new(idx, 'X') : Frame.new(idx, '1', '5'))
+    frames = Array.new(10) do |idx|
+      idx < 2 ? Frame.new(idx, 'X') : Frame.new(idx, '1', '5')
     end
 
     assert_equal 21, frames.first.score(frames)
   end
 
   def test_score_double_strike_at_final_two_frames
-    frames = []
-    10.times do |idx|
-      frames << if idx == 8
-                  Frame.new(idx, 'X')
-                elsif idx == 9
-                  Frame.new(idx, 'X', '1', '5')
-                else
-                  Frame.new(idx, '1', '5')
-                end
+    frames = Array.new(10) do |idx|
+      if idx == 8
+        Frame.new(idx, 'X')
+      elsif idx == 9
+        Frame.new(idx, 'X', '1', '5')
+      else
+        Frame.new(idx, '1', '5')
+      end
     end
 
     assert_equal 21, frames[8].score(frames)
