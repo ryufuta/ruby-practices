c823b32fca0e9a3d661d8e505dfc19676e0984b9
ryufuta <rfutago@gmail.com>
2024-09-30 16:11:46 +0900

    Remove an unnecessary test case

--- 06.bowling_object/test/shot_test.rb
+++ 06.bowling_object/test/shot_test.rb
@@ -14,11 +14,6 @@ class ShotTest < Minitest::Test
     assert_equal 10, shot.score
   end
 
-  def test_score_nil
-    shot = Shot.new(nil)
-    assert_equal 0, shot.score
-  end
-
   def test_strike
     shot = Shot.new('X')
     assert shot.strike?
